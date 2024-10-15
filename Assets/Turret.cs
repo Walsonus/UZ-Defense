8be2b83cc24e48aa9c64d87dfff488673c09890d
Krzysztof <k.sokalski@onet.pl>
2024-10-15 20:24:33 +0200

    rework targetting system, fix targetting when out of range, add arrow sprite

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -9,11 +9,20 @@ public class Turret : MonoBehaviour
     [Header("References")]
     //point of rotation of a turret's weapon
     [SerializeField] private Transform rotationPoint;
+    //project layer with enemies
     [SerializeField] private LayerMask enemyMask;
-
+    //reference to a bullet object
+    [SerializeField] private GameObject bullet;
+    //spawn point for bullets on the turret
+    [SerializeField] private Transform bulletPoint;
+    
     [Header("Attribute")]
     //targetting range of the turret
     [SerializeField] private float range = 4f;
+    //rotation speed of a turret
+    [SerializeField] private float rotationSpeed = 100f;
+    //attack speed of the turret in shoots per second
+    [SerializeField] private float fireRate = 2f;
 
     //test function showing the range of the turret
         private void OnDrawGizmosSelected(){
@@ -23,6 +32,8 @@ public class Turret : MonoBehaviour
 
     //position of the turret target
     private Transform target;
+
+    private float timeToNextBullet;
 
 
     // Start is called before the first frame update
@@ -35,16 +46,40 @@ public class Turret : MonoBehaviour
     void Update()
     {
         if(target == null){
-            findTarget();
+            FindTarget();
             return;
         }
         
-        rotateToTarget();
+        RotateToTarget();
+
+        //if the target goes out of range, change target
+        if(!IsTargetInRange()){
+            target = null;
+
+        //if the target is in range, shoot
+        } else{
+            
+            timeToNextBullet += Time.deltaTime;
+
+            //check if it is time to shoot
+            if(timeToNextBullet >= 1f/fireRate){
+                Shoot();
+                timeToNextBullet = 0f;
+            }
+            
+        }
 
     }
 
+    //returns true if the distance between the target and the turret is smaller or equal turret range
+    private bool IsTargetInRange(){
+
+        if (Vector2.Distance(transform.position, target.position) <= range) return true;
+        else return false; 
+    }
+
     //function for finding a target
-    private void findTarget(){
+    private void FindTarget(){
 
         //array of rays confirming a "hit" on an enemy
         RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, range, (Vector2) transform.position, 0f, enemyMask);
@@ -55,15 +90,20 @@ public class Turret : MonoBehaviour
         }
     }
 
-    private void rotateToTarget(){
+    private void RotateToTarget(){
         //calculate the angle in radians between the current position and the target's position
         float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
 
         //create a Quaternion rotation based on the calculated angle, rotating around the Z-axis
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
 
-        //applying the rotation to the turret
-        rotationPoint.rotation = targetRotation;
+        //applying the rotation to the turret (deltaTime added to remove FPS count affect on rotation speed)
+        rotationPoint.rotation = Quaternion.RotateTowards(rotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //rotationPoint.rotation = targetRotation;
+    }
+
+    private void Shoot(){
+        Debug.Log("Shoot");
     }
 
 }
