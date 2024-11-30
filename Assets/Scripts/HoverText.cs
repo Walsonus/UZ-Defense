6cd441f1d5a3543a27b03632203323cdda5c6edf
Walsonus <walkiewiczbartosz@gmail.com>
2024-11-30 18:05:24 +0100

    add music, upgrade hovering system for shop menu

--- Assets/Scripts/HoverText.cs
+++ Assets/Scripts/HoverText.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;  // Potrzebne do obsługi interakcji z myszką
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
-public class HoverText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class HoverText : MonoBehaviour,IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("References")]
     [SerializeField] private TextMeshProUGUI textMeshPro; // Referencja do TMP
     [SerializeField] private Color hoverColor = Color.yellow; // Kolor podświetlenia
     private Color originalColor; // Oryginalny kolor tekstu
+    private bool isSelected = false;
+    private static HoverText currentlySelected;
 
     private void Start()
     {
@@ -19,13 +23,31 @@ public class HoverText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
     public void OnPointerEnter(PointerEventData eventData)
     {
         // Zmieniamy kolor tekstu na podświetlony
+        if(currentlySelected != this){
         textMeshPro.color = hoverColor;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Zmieniamy kolor tekstu na podświetlony
+        if(currentlySelected != null && currentlySelected != this){
+            currentlySelected.OnDeselect();
+        }
+        textMeshPro.color = hoverColor;
+        currentlySelected = this;
     }
 
     // Obsługuje opuszczenie obszaru tekstu
     public void OnPointerExit(PointerEventData eventData)
     {
         // Przywracamy oryginalny kolor
+        if(currentlySelected != this){
+        textMeshPro.color = originalColor;
+        }
+    }
+
+    public void OnDeselect(){
         textMeshPro.color = originalColor;
     }
 }
